bd42d237e7c8c29f5c023046a04597a41cd529c7
Erin Jackson <79530284+erinbjackson@users.noreply.github.com>
2026-06-23 07:21:07 -0700

    Update assistant message and system prompt

--- project1/agent_yard_project.py
+++ project1/agent_yard_project.py
@@ -3,7 +3,7 @@ from basic_llm import LLM
 with open("data/gardenInstruct.md", "r", encoding="utf-8") as file: # obtain proprietary docs
    documentation = file.read()
 
-assistant_message = "Welcome to The Garden Master! How can I help?" # new intro message
+assistant_message = "Hello! I am Garden Master of Gardenville! How can I help?" # new intro message
 user_input = input(f"\nAssistant: {assistant_message}\n\nUser: ")
 
 history = [
@@ -14,7 +14,7 @@ history = [
 while user_input != "exit":
     response = LLM().generate(
         messages_prompt=history,
-        system_prompt=f"""You are a garden master. Your job is to give garden instructions based on the following documentation: {documentation}"""  # new system prompt
+        system_prompt=f"""You are a garden master in the land of Gardenville. Your job is to give garden instructions based solely on the following documentation: {documentation}"""  # new system prompt
     )
 
     llm_response_text = f"\nAssistant: {response["text"]}"
